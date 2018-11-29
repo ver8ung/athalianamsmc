8ce1e17df40f746e5e3a49c5426094019748236c
ver8ung <45457029+ver8ung@users.noreply.github.com>
2018-11-29 15:38:09 +0100

    Add files via upload

--- runmsmc2.py
+++ runmsmc2.py
@@ -13,8 +13,6 @@ input_directory = "/data/home/users/m.ruscheweyh/pythontest/input/"
 haplotypes_madeira = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
 for filename in os.listdir(input_directory):
     if filename.endswith(".multihetsep.txt"):
-        call (["./msmc_1.1.0_linux64bit","-I0,1","-p1*2+15*1+1*2","-o",outdir+filename,filename])
+        call (["./msmc2_linux64bit","-I0,1","-p1*2+15*1+1*2","-o",outdir+filename,filename])
     else:
          print("Specify valid input files.")
-    
-    
