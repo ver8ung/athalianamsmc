92ca54b0a5f3468228732f68265bfd020e0d40d1
ver8ung <45457029+ver8ung@users.noreply.github.com>
2018-11-29 13:12:07 +0100

    Add files via upload

---
+++ msmcfunction.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 12:45:58 2018
+
+@author: m.ruscheweyh
+"""
+
+def run_msmc1(path,inputname,outputname,fix=true,prior=0.25,index,singlechr=true):
+    "This function runs MSMC_1.0.0.1 on the designated input with chosen parameters"
+    cmd = 
+    
