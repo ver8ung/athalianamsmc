9e096c261b8163d2a648eb864bc0a87a5731b719
ver8ung <45457029+ver8ung@users.noreply.github.com>
2018-12-14 12:53:17 +0100

    current

--- raw_scripts/allchrtemplate.py
+++ raw_scripts/allchrtemplate.py
@@ -7,7 +7,7 @@
 Created on Thu Nov 29 11:26:54 2018
 @author: m.ruscheweyh
 """
-import os, glob
+import os, glob, fnmatch
 from subprocess import call
   
 input_directory = "/data/home/users/m.ruscheweyh/pythontest/input/" # specify input directory
@@ -33,6 +33,7 @@ all_chr_input_tubingencent = glob.glob("/data/home/users/m.ruscheweyh/pythontest
 all_chr_input_tubingencent_reduced = glob.glob("/data/home/users/m.ruscheweyh/pythontest/input/tubingencent*.multihetsep_reduced.txt")
 all_chr_input_tubingeneastlust = glob.glob("/data/home/users/m.ruscheweyh/pythontest/input/tubingeneastlust*.multihetsep.txt")
 all_chr_input_tubingeneastlust_reduced = glob.glob("/data/home/users/m.ruscheweyh/pythontest/input/tubingeneastlust*.multihetsep_reduced.txt")
+
 hap_counter = 1     # counter for iteration over haplotype combinations, determines file output prefix, default prefix "01_"
 index_counter = 0   # counters for the -I parameter of MSMC, iterates over x-amount of haplotypes in pairs of x-indexes
 index_counter_1 = 1 #######################################################################################################
@@ -50,10 +51,10 @@ r_chr4 = 5.4286
 r_chr5 = 5.1429
 r_average =  5.1143
 
-for filename in os.listdir(input_directory): # retrieve filenames in input_directory for output prefix
-    if filename.endswith(".multihetsep.txt"): # call MSMC with desired parameters on specified input files
+for file in os.listdir(input_directory): # retrieve filenames in input_directory for output prefix
+    if fnmatch.fnmatch(file, "*madeira*.txt"): # call MSMC with desired parameters on specified input files
       while hap_counter < 27:
-              outputprefix = filename[:11]   # use only desired positions of input filenames as output prefix
+              outputprefix = file[:13]   # use only desired positions of input filenames as output prefix
               call (["./msmc_1.1.0_linux64bit","-I","%s,%s" %(index_counter,index_counter_1),"-R","-r",str(r_average),"-o",outdir_allchr+'2hap_rhofixed_allchr'+'%02d_%s' %(hap_counter,outputprefix),"%s" %(all_chr_input_madeira[0]),"%s" %(all_chr_input_madeira[1]),"%s" %(all_chr_input_madeira[2]),"%s" %(all_chr_input_madeira[3]),"%s" %(all_chr_input_madeira[4])])
               hap_counter += 1
               index_counter += 2
@@ -65,11 +66,11 @@ for filename in os.listdir(input_directory): # retrieve filenames in input_direc
               index_counter_1 = 1
               continue
           
-for filename in os.listdir(input_directory): # retrieve filenames in input_directory for output prefix       
-    if filename.endswith(".multihetsep_reduced.txt"): # call MSMC with desired parameters on specified input files
+for file in os.listdir(input_directory): # retrieve filenames in input_directory for output prefix       
+     if fnmatch.fnmatch(file, "*madeira*_reduced*.txt"): # call MSMC with desired parameters on specified input files
       while hap_counter < 27:
-              outputprefix = filename[:31]   # use only desired positions of input filenames as output prefix
-              call (["./msmc_1.1.0_linux64bit","-I","%s,%s,%s,%s" %(index_counter,index_counter_1,index_counter_2,index_counter_3),"-R","-r",str(r_average),"-o",outdir+'4hap_fixeddef_nocent'+'%02d_%s' %(hap_counter,outputprefix),"%s" %(all_chr_input_madeira[0]),"%s" %(all_chr_input_madeira[1]),"%s" %(all_chr_input_madeira[2]),"%s" %(all_chr_input_madeira[3]),"%s" %(all_chr_input_madeira[4])])
+              outputprefix = file[:31]   # use only desired positions of input filenames as output prefix
+              call (["./msmc_1.1.0_linux64bit","-I","%s,%s,%s,%s" %(index_counter,index_counter_1,index_counter_2,index_counter_3),"-R","-r",str(r_average),"-o",outdir+'4hap_fixeddef_nocent'+'%02d_%s' %(hap_counter,outputprefix),"%s" %(all_chr_input_madeira_reduced[0]),"%s" %(all_chr_input_madeira_reduced[1]),"%s" %(all_chr_input_madeira_reduced[2]),"%s" %(all_chr_input_madeira_reduced[3]),"%s" %(all_chr_input_madeira_reduced[4])])
               hap_counter += 1
               index_counter += 4
               index_counter_1 += 4
