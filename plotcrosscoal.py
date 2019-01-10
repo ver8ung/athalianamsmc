8004859eeedd7eeb47811a829a915539e144c7ac
ver8ung <45457029+ver8ung@users.noreply.github.com>
2019-01-10 15:33:25 +0100

    placeholder, work in progress
    plotting results in python

---
+++ plotcrosscoal.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 10 15:28:09 2019
+
+@author: m.ruscheweyh
+"""
+
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Parameters for Arabidopsis thaliana
+mu = 7e-9
+gen = 1
+####################################
+
+os.chdir("/data/home/users/m.ruscheweyh/msmc/crosscoal/results/combined")
+###################################
+#plotting##########################
+
+crosscoal_Spain_Mad = pd.read_csv("spain_mad_combined.txt", delim_whitespace=True)
+crosscoal_Spain_Tub = pd.read_csv("spain_tub_combined.txt", delim_whitespace=True)
+crosscoal_Tub_Swe = pd.read_csv("spain_tub_combined.txt", delim_whitespace=True)
+plt.step(crosscoal_Spain_Mad["left_time_boundary"]/mu*gen, 2 * crosscoal_Spain_Mad["lambda_01"] / (crosscoal_Spain_Mad["lambda_00"] + crosscoal_Spain_Mad["lambda_11"]))
+plt.step(crosscoal_Spain_Tub["left_time_boundary"]/mu*gen, 2 * crosscoal_Spain_Mad["lambda_01"] / (crosscoal_Spain_Tub["lambda_00"] + crosscoal_Spain_Mad["lambda_11"]))
+plt.step(crosscoal_Tub_Swe["left_time_boundary"]/mu*gen, 2 * crosscoal_Tub_Swe["lambda_01"] / (crosscoal_Tub_Swe["lambda_00"] + crosscoal_Spain_Mad["lambda_11"]))
+plt.xlim(1000,500000)
+plt.xlabel("years ago");
+plt.ylabel("relative cross coalescence rate");
+plt.gca().set_xscale('log')
