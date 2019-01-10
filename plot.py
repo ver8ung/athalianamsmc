12e6431783972ca9456472cad54e8917703689c6
ver8ung <45457029+ver8ung@users.noreply.github.com>
2019-01-10 16:31:46 +0100

    working minimal examples of plotting scripts
    script that allows simple plot of MSMC output files, generating a png image file

--- plot.py
+++ plot.py
@@ -8,6 +8,7 @@ Created on Thu Jan 10 15:10:33 2019
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.pyplot import figure
 
 #Parameters for Arabidopsis thaliana
 mu = 7e-9
@@ -20,11 +21,15 @@ os.chdir("/data/home/users/m.ruscheweyh/msmc/pop_results_wholechr_allchr")
 
 Mad2hap = pd.read_csv("Madeira2hapfd_pop1fixeddef_all.msmc2.final.txt", delim_whitespace=True)
 Spain2hap = pd.read_csv("Spain/spain2hapfd_pop1fixeddef_all.msmc2.final.txt", delim_whitespace=True)
+
+figure(num=None, figsize=(10, 8), dpi=100, facecolor='w', edgecolor='k')
 plt.step(Mad2hap["left_time_boundary"]/mu*gen, (1/Mad2hap["lambda"])/(2*mu), label="Madeira_2haplotypes_fixeddef")
 plt.step(Spain2hap["left_time_boundary"]/mu*gen, (1/Spain2hap["lambda"])/(2*mu), label="Spain_2haplotypes_fixeddef")
-plt.ylim(15000,400000)
+plt.ylim(50000,800000)
 plt.xlabel("years ago");
 plt.ylabel("effective population size");
 plt.gca().set_xscale('log')
-plt.legend()
+plt.legend(loc=0)
+
+os.chdir("/data/home/users/m.ruscheweyh/MSMCresults/pyplots")
 plt.savefig("2haplotypes.png")
--- plotcrosscoal.py
+++ plotcrosscoal.py
@@ -9,6 +9,7 @@ Created on Thu Jan 10 15:28:09 2019
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.pyplot import figure
 
 #Parameters for Arabidopsis thaliana
 mu = 7e-9
@@ -22,6 +23,8 @@ os.chdir("/data/home/users/m.ruscheweyh/msmc/crosscoal/results/combined")
 crosscoal_Spain_Mad = pd.read_csv("spain_mad_combined.txt", delim_whitespace=True)
 crosscoal_Spain_Tub = pd.read_csv("spain_tub_combined.txt", delim_whitespace=True)
 crosscoal_Tub_Swe = pd.read_csv("spain_tub_combined.txt", delim_whitespace=True)
+
+figure(num=None, figsize=(10, 8), dpi=100, facecolor='w', edgecolor='k')
 plt.step(crosscoal_Spain_Mad["left_time_boundary"]/mu*gen, 2 * crosscoal_Spain_Mad["lambda_01"] / (crosscoal_Spain_Mad["lambda_00"] + crosscoal_Spain_Mad["lambda_11"]))
 plt.step(crosscoal_Spain_Tub["left_time_boundary"]/mu*gen, 2 * crosscoal_Spain_Mad["lambda_01"] / (crosscoal_Spain_Tub["lambda_00"] + crosscoal_Spain_Mad["lambda_11"]))
 plt.step(crosscoal_Tub_Swe["left_time_boundary"]/mu*gen, 2 * crosscoal_Tub_Swe["lambda_01"] / (crosscoal_Tub_Swe["lambda_00"] + crosscoal_Spain_Mad["lambda_11"]))
@@ -29,3 +32,5 @@ plt.xlim(1000,500000)
 plt.xlabel("years ago");
 plt.ylabel("relative cross coalescence rate");
 plt.gca().set_xscale('log')
+plt.legend()
+plt.savefig("foo.png")
