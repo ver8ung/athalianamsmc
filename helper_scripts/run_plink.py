f733ef33d44a8f9674b686626fcb7dfb6e1c83e1
ver8ung <45457029+ver8ung@users.noreply.github.com>
2019-01-16 18:07:17 +0100

    generalized minimal example
    this script converts vcf files in the input directory to plink-bed by calling on the plink software
    and outputs it to a separate directory, iterating over every file in the input directory

--- helper_scripts/run_plink.py
+++ helper_scripts/run_plink.py
@@ -13,12 +13,34 @@ input_directory = "/data/home/users/m.ruscheweyh/plink/input"
 outdir = "/data/home/users/m.ruscheweyh/plink/output"
 
 madeira_samps = glob.glob("/data/home/users/m.ruscheweyh/plink/input/*madeira*.vcf.gz")
-madeiran_loop = 0
 
 for file in os.listdir(input_directory):
     if fnmatch.fnmatch(file, "*madeira*.vcf.gz"):
-        while madeiran_loop < len(madeira_samps):
-              outputprefix = str(madeira_samps[madeiran_loop])[42:55]   
-              call (["./plink","--threads","8","--make-bed","--allow-extra-chr","--chr","1-5","--vcf","%s" %(madeira_samps[madeiran_loop]), "--out","%s/%s" %(outdir,outputprefix)])
-              madeiran_loop +=1
-              continue
+              outputprefix = str(madeira_samps[0])[42:55]   
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[0]), "--out","%s" %(outputprefix)])
+              outputprefix = str(madeira_samps[1])[42:55]
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[1]), "--out","%s" %(outputprefix)])
+              outputprefix = str(madeira_samps[2])[42:55]
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[2]), "--out","%s" %(outputprefix)])
+              outputprefix = str(madeira_samps[3])[42:55]
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[3]), "--out","%s" %(outputprefix)])
+              outputprefix = str(madeira_samps[4])[42:55]
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[4]), "--out","%s" %(outputprefix)])
+              outputprefix = str(madeira_samps[5])[42:55]
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[5]), "--out","%s" %(outputprefix)])
+              outputprefix = str(madeira_samps[6])[42:55]
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[6]), "--out","%s" %(outputprefix)])
+              outputprefix = str(madeira_samps[7])[42:55]
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[7]), "--out","%s" %(outputprefix)])
+              outputprefix = str(madeira_samps[8])[42:55]
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[8]), "--out","%s" %(outputprefix)])
+              outputprefix = str(madeira_samps[9])[42:55]
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[9]), "--out","%s" %(outputprefix)])
+              outputprefix = str(madeira_samps[10])[42:55]
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[10]), "--out","%s" %(outputprefix)])
+              outputprefix = str(madeira_samps[11])[42:55]
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[11]), "--out","%s" %(outputprefix)])
+              outputprefix = str(madeira_samps[12])[42:55]
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[12]), "--out","%s" %(outputprefix)])
+              outputprefix = str(madeira_samps[13])[42:55]
+              call (["./plink","--threads 8", "--make-bed","--allow-extra-chr","--chr 1-5","--vcf","%s" %(madeira_samps[13]), "--out","%s" %(outputprefix)])
